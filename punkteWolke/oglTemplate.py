849a24384fca3ab9a89245218b6c753080a13516
Moritz Bantleon <moritz@bantleon.me>
2019-06-02 11:50:25 +0200

    Rotation works: but the whole koord. system gets rotated

--- punkteWolke/oglTemplate.py
+++ punkteWolke/oglTemplate.py
@@ -18,6 +18,17 @@ def init(width, height):
    glOrtho(-1.5, 1.5, -1.5, 1.5, -1.0, 1.0) #multiply with new p-matrix
    glMatrixMode(GL_MODELVIEW)               #switch to modelview matrix
 
+   #Camera
+   gluPerspective(30, 1, 1, 50)
+   gluLookAt(0, 0, 4, 0, 0, 0, 0, 1, 0)
+
+   # Transform in koo.-sys.
+   #glOrtho(bbox["left"], bbox["right"], bbox["bottom"], bbox["top"], bbox["near"], bbox["far"])
+
+
+
+
+
 
 def display():
    global points
@@ -25,79 +36,12 @@ def display():
    glClear(GL_COLOR_BUFFER_BIT) #clear screen
    glColor(0.0, 0.0, 1.0)       #render stuff
 
-   #Bounding Box
-   vecX = [vec[0] for vec in points]
-   vecY = [vec[1] for vec in points]
-   vecZ = [vec[2] for vec in points]
-
-   bbox = {"right": max(vecX), "left": min(vecX),
-           "top": max(vecY), "bottom": min(vecY),
-           "far": max(vecZ), "near": min(vecZ)}
-
-   #Camera
-   gluPerspective(30, 1, 1, 100)
-   gluLookAt(0, 0, 4, 0, 0, 0, 0, 1, 0)
-
-   #Transform in koo.-sys.
-   glOrtho(bbox["left"], bbox["right"], bbox["bottom"], bbox["top"], bbox["near"], bbox["far"])
-
-   ''' TypeError with Frustum
-   bboxMedian = [np.median([bbox["right"], bbox["left"]]), np.median([bbox["top"], bbox["bottom"]]),
-                 np.median([bbox["far"], bbox["near"]])]
-   fNear = bboxMedian[0], bboxMedian[1], bbox["near"]
-   fFar = bboxMedian[0], bboxMedian[1], bbox["far"]
-   glFrustum(bbox["left"], bbox["right"], bbox["bottom"], bbox["top"], fNear, fFar)
-   '''
-
-
-
-   ''' Draw Triangles 
-   glBegin(GL_TRIANGLE_STRIP)
-
-
-   h = math.sqrt(0.75)/7
-   j = 0
-   for i in range(5):
-
-      i = i/7
-
-      if i == 2/7:
-
-         # Tri look up
-         glVertex2f(-1 / 7 + i, -1 / 7 + j)
-         glVertex2f(0 + i, -1 / 7 + j)
-         glVertex2f(-0.5 / 7 + i, h - 1 / 7 + j)
-
-         # Tri look down
-         glVertex2f(0 + i, -1 / 7 + j)
-         glVertex2f(-0.5 / 7 + i, h - 1 / 7 + j)
-         glVertex2f(0.5 / 7 + i, h - 1 / 7 + j)
-
-         j = h
-         i += 1/7
-
-
-      #Tri look up
-      glVertex2f(-1/7 +i, -1/7 +j)
-      glVertex2f(0 +i, -1/7 +j)
-      glVertex2f(-0.5/7 +i, h-1/7 +j)
-
-      #Tri look down
-      glVertex2f(0 +i, -1/7 +j)
-      glVertex2f(-0.5/7 +i, h-1/7 +j)
-      glVertex2f(0.5/7 +i, h-1/7 +j)
-
-
-
-   glEnd();
-   '''
-
    #draw
    glBegin(GL_POINTS)
 
    for xyz in points:
       x, y, z = xyz
-      glVertex3f(x, y, z)
+      glVertex3f(x, y, z) #FIXME with v a Attribute Error occures
 
    glEnd()
 
@@ -121,10 +65,30 @@ def reshape(width, height):
 
 
 def keyPressed(key, x, y):
-   """ handle keypress events """
-   if key == chr(27): # chr(27) = ESCAPE
-       sys.exit()
+   key = str(key)
+   key = key.split('\'')[1]
+   """ handle keypress events """ #TODO
+   if key == chr(27) or key == chr(113): # chr(27) = ESCAPE chr(113) = q
+       sys.exit(0)
+   elif key == chr(120):
+      print("i must rotate around the x-axis ccounterlockwise")
+      glRotate(2, 1, 0, 0)
 
+   elif key == chr(121):
+      print("i must rotate around the y-axis counterclockwise")
+      glRotate(2, 0, 1, 0)
+
+   elif key == chr(122):
+      print("i must rotate around the z-axis counterclockwise")
+      glRotate(2, 0, 0, 1)
+
+   else:
+      print("What is this: ", key)
+
+   display()
+
+def rotateBy(axis): # axis 0, 1, 2 eventl
+   print("i should rotate animals here, but i cant see animals out here ... ")
 
 def mouse(button, state, x, y):
    """ handle mouse events """
@@ -145,17 +109,39 @@ def menu_func(value):
    glutPostRedisplay()
 
 
+def process_input(filepath):
+   global points
+   with open(filepath, "r") as file:
+      for line in file.readlines():
+         line = line.split()
+         points.append(np.array([float(line[0]), float(line[1]), float(line[2])]))
+
+   # Bounding Box
+   vecX = [vec[0] for vec in points]
+   vecY = [vec[1] for vec in points]
+   vecZ = [vec[2] for vec in points]
+
+   bbox = {"right": max(vecX), "left": min(vecX),
+            "top": max(vecY), "bottom": min(vecY),
+            "far": max(vecZ), "near": min(vecZ)}
+
+   #Put the middle of the Boundingbox in the origin
+   bboxMedian = [np.median([bbox["right"], bbox["left"]]), np.median([bbox["top"], bbox["bottom"]]),
+                 np.median([bbox["far"], bbox["near"]])]
+   points = points - np.array([bboxMedian[0], bboxMedian[1], bboxMedian[2]])
+
+   #scale in to[-1, 1]^3 | scale factor is 2/maxDiagonal
+   maxVec = max([bbox["right"] - bbox["left"], bbox["top"] - bbox["bottom"], bbox["far"] - bbox["near"]])
+   points = points * (2.0 / maxVec)
+
+
 def main():
    # Hack for Mac OS X
    cwd = os.getcwd()
    glutInit(sys.argv)
    os.chdir(cwd)
 
-   global points
-   with open(sys.argv[1], "r") as file:
-      for line in file.readlines():
-         line = line.split()
-         points.append(np.array([float(line[0]), float(line[1]), float(line[2])]))
+   process_input(sys.argv[1])
 
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
    glutInitWindowSize(500, 500)
@@ -167,8 +153,6 @@ def main():
    glutMouseFunc(mouse)         #register mouse function
    glutMotionFunc(mouseMotion)  #register motion function
    glutCreateMenu(menu_func)    #register menue function
-
-
 
 
    glutAddMenuEntry("First Entry",FIRST) #Add a menu entry
